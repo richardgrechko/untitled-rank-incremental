875ebf1dc863ce6be8085559082665ffd1dfc13d
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-09-13 19:30:19 +0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,12 +1,12 @@
 const game = {
 	points: new Decimal(0),
-	rank: new Decimal(1),
+	ranks: new Decimal(1),
 	rankReqs: new Decimal(1000000),
 	rankGain: new Decimal(0),
-	tier: new Decimal(1),
+	tiers: new Decimal(1),
 	tierReqs: new Decimal(5),
 	tierGain: new Decimal(0),
-	tetr: new Decimal(1),
+	tetrs: new Decimal(1),
 	tetrReqs: new Decimal(5),
 	tetrGain: new Decimal(0),
 };
