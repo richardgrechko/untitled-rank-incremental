b38160a58b2a81e6821ac1088b8b6251077d58cb
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-09-13 18:34:17 +0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const data = {
+	points: new Decimal(0),
+	rank: new Decimal(1),
+	rankRequirement: new Decimal(1000000),
+	rankGain: new Decimal(0),
+	tier: new Decimal(1),
+	tierRequirement: new Decimal(5),
+	tierGain: new Decimal(0),
+	tetr: new Decimal(1),
+	tetrRequirement: new Decimal(5),
+	tetrGain: new Decimal(0),
+};
