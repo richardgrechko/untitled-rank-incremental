65361f425331b4ebdeeacb21caac597b747ba6ca
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-09-13 19:16:45 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -54,5 +54,33 @@ const functions = {
 			game.tetrReqs = loadVal(E(obj.tetrReqs), E(5));
 			game.tetrGain = loadVal(E(obj.tetrGain), E(0));
 		}
-	}
+	},
+	format(options) {
+		function commaFormat(num) {
+			let portions = num.split(".")
+			portions[0] = portions[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
+			if (portions.length == 1)
+				return portions[0]
+			return portions[0] + "." + portions[1]
+		}
+		options = {
+			num: options.num || new Decimal("1"),
+			precision: options.precision ?? 3,
+		}
+		if (options.num.gte("10^^1e6")) {
+			return "F" + fullFormat({num: options.num.slog().floor(), precision: 0});
+		} else if (options.num.gte("10^^10")) {
+			return fullFormat({num: new Decimal(10).pow(options.num.slog().sub(options.num.slog().floor())), precision: 3}) + "F" + fullFormat({num: options.num.slog().floor(), precision: 0});
+		} else if (options.num.gte("ee10")) {
+			return "e" + fullFormat({num: options.num.log10().floor(), precision: 0});
+		} else if (options.num.gte("e10")) {
+			return fullFormat({num: new Decimal(10).pow(options.num.log10().sub(options.num.log10().floor())), precision: 3}) + "e" + fullFormat({num: options.num.log10().floor(), precision: 0});
+		} else if (options.num.gte("1000000")) {
+			return commaFormat(options.num.floor().toString());
+		} else if (options.num.gte("1000")) {
+			return commaFormat(Number(options.num).toFixed(options.precision));
+		} else if (options.num.gte("0")) {
+			return Number(options.num).toFixed(options.precision);
+		};
+	},
 }
