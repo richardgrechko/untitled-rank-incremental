be964b0d148b958ce6753df6046288961cd4d356
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-09-13 20:00:35 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let dt1 = Date.now(), dt2 = Date.now(), dt;
+function E(n) {
+	return new Decimal(n)
+}
 let update = function()
 {
 	dt1 = Date.now();
@@ -6,21 +9,18 @@ let update = function()
 	dt2 = Date.now();
 	game.points = game.points.add(game.pointGain.mul(dt));
 	game.rankGain = game.points.div(1000000).mul(5).log(5).pow(1/1.05).floor();
-	game.rankReqs = new Decimal(1000000).mul(new Decimal(5).pow(game.ranks.pow(1.05)));
-	game.nextRank = new Decimal(1000000).mul(new Decimal(5).pow(game.ranks.add(game.rankGain).pow(1.05)));
+	game.rankReqs = E(1000000).mul(E(5).pow(game.ranks.pow(1.05)));
+	game.nextRank = E(1000000).mul(E(5).pow(game.ranks.add(game.rankGain).pow(1.05)));
 	game.tierGain = game.ranks.pow(1/1.25).div(5).floor();
-	game.tierReqs = new Decimal(5).mul(game.tiers.pow(1.25)).add(0.5).floor();
-	game.nextTier = new Decimal(5).mul(game.tiers.add(game.tierGain).pow(1.25)).add(0.5).floor();
+	game.tierReqs = E(5).mul(game.tiers.pow(1.25)).add(0.5).floor();
+	game.nextTier = E(5).mul(game.tiers.add(game.tierGain).pow(1.25)).add(0.5).floor();
 	game.tetrGain = game.tiers.pow(1/1.5).div(5).floor();
-	game.tetrReqs = new Decimal(5).mul(game.tetrs.pow(1.5)).add(0.5).floor();
-	game.nextTetr = new Decimal(5).mul(game.tetrs.add(game.tetrGain).pow(1.5)).add(0.5).floor();
-	game.rankGain = (game.rankGain == new Decimal(NaN)) ? new Decimal(0) : game.rankGain;
-	game.tierGain = (game.tierGain == new Decimal(NaN)) ? new Decimal(0) : game.tierGain;
-	game.tetrGain = (game.tetrGain == new Decimal(NaN)) ? new Decimal(0) : game.tetrGain;
+	game.tetrReqs = E(5).mul(game.tetrs.pow(1.5)).add(0.5).floor();
+	game.nextTetr = E(5).mul(game.tetrs.add(game.tetrGain).pow(1.5)).add(0.5).floor();
+	game.rankGain = (game.rankGain == E(NaN)) ? E(0) : game.rankGain;
+	game.tierGain = (game.tierGain == E(NaN)) ? E(0) : game.tierGain;
+	game.tetrGain = (game.tetrGain == E(NaN)) ? E(0) : game.tetrGain;
 	requestAnimationFrame(update);
-}
-function E(n) {
-	return new Decimal(n)
 }
 let onCreate = function()
 {
